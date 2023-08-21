dde4d2bc3be93a8c0525e635480d188545325451
otorquati <torquati.osvaldo@gmail.com>
2023-08-21 18:19:19 -0300

    starting the project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main className='centralize'>
+      <form action="">
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Senha' />
+        <button type='button' className='clear'>Recuperar senha</button>
+        <button type='button' className='solid'>Entrar</button>
+        <button type='button' className='outline'>Registrar</button>
+      </form>
+    </main>
+  );
+}
+
+export default App;
