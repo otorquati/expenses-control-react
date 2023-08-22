762919ce4514a06d4817d4001d555a446d959656
otorquati <torquati.osvaldo@gmail.com>
2023-08-22 16:42:32 -0300

    finishing 1st part of TDD (Test Driven Development) for login

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 
 describe('Login', () => {
-  // Testa se o campo de email não está vazio
+  // 1- Testa se o campo de email não está vazio
   test('given email, when empty, then show required error message', async () => {
     render(<App />);
 
@@ -17,7 +17,7 @@ describe('Login', () => {
     expect(requiredError).not.toBeNull();
 
   })
-  // Testa se o campo de email foi preenchido
+  // 2- Testa se o campo de email foi preenchido
   test('given email, when has value, then show required error message', async () => {
     render(<App />);
 
@@ -29,7 +29,7 @@ describe('Login', () => {
 
   })
   
-  // Testa se o campo de email foi modificado
+  // 3- Testa se o campo de email foi modificado
   test('given email, when field not changed, then hide required error message', async () => {
     render(<App />);
 
@@ -38,7 +38,7 @@ describe('Login', () => {
 
   })
 
-   // Testa se o campo de email é inválido
+   //4- Testa se o campo de email é inválido
    test('given email, when invalid, then show invalid error message', async () => {
     render(<App />);
 
@@ -49,7 +49,7 @@ describe('Login', () => {
     expect(requiredError).not.toBeNull();
 
   })
-   // Testa se o campo de Senha não está vazio
+   //5- Testa se o campo de Senha não está vazio
    test('given password, when empty, then show required error message', async () => {
     render(<App />);
 
@@ -61,7 +61,7 @@ describe('Login', () => {
     expect(requiredError).not.toBeNull();
 
   })
-     // Testa se o campo de Senha não está vazio
+     //6- Testa se o campo de Senha não está vazio
      test('given password, when not empty, then hide required error message', async () => {
       render(<App />);
   
@@ -72,4 +72,46 @@ describe('Login', () => {
       expect(requiredError).toBeNull();
   
     })
+    //7- Testa se o Email digitado é invalido e desabilita o botão recuperar senha
+    test('given email, when empty, then disable recover password button', () => {
+      render (<App />);
+
+      const recoverPasswordButton = screen.getByTestId('recover-password-button');
+
+      expect(recoverPasswordButton).toBeDisabled();
+    })
+
+    //8- Testa se o Email digitado é válido e habilita o botão recuperar senha
+    test('given email, when valid, then enable recover password button', () => {
+      render (<App />);
+
+      const email = screen.getByTestId('email');
+      userEvent.type(email,"valid@email.com");
+
+      const recoverPasswordButton = screen.getByTestId('recover-password-button');
+
+      expect(recoverPasswordButton).not.toBeDisabled();
+    })
+
+    //9- Testa os dados do Formulário  são inválidos e habilita o botão Entrar
+    test('given form invalid, then disable login button', () => {
+    render (<App />);
+
+    const loginButton = screen.getByTestId('login-button');
+
+    expect(loginButton).toBeDisabled();
+      })
+
+    //10 - Testa os dados do Formulário são válidos e habilita o botão Entrar
+    test('given form valid, then enable login button', () => {
+      render (<App />);
+
+      const email = screen.getByTestId('email');
+      userEvent.type(email,"valid@email.com");
+      const password = screen.getByTestId('password');
+      userEvent.type(password, "anyValue");
+      const loginButton = screen.getByTestId('login-button');
+
+      expect(loginButton).not.toBeDisabled();
+        })  
 })
--- src/App.tsx
+++ src/App.tsx
@@ -38,13 +38,20 @@ function App() {
           onChange={event => setForm({...form, password: {
             hasChanged: true, value: event.target.value 
           }})}
-          data-testid='password' />
+          data-testid='password' 
+          />
         { 
         form.password.hasChanged && !form.password.value 
           && <div data-testid="password-required">Senha é obrigatória</div>
         }
-        <button type='button' className='clear'>Recuperar senha</button>
-        <button type='button' className='solid'>Entrar</button>
+        <button type='button' className='clear'
+        data-testid = "recover-password-button"
+        disabled = {!isEmailValid(form.email.value)}>
+        Recuperar senha</button>
+        <button type='button' className='solid'
+        data-testid="login-button"
+        disabled = {!isEmailValid(form.email.value) || !form.password.value}
+        >Entrar</button>
         <button type='button' className='outline'>Registrar</button>
       </form>
     </main>
