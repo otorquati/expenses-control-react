97be5c264a90a9629c5c9e98c36654e817629de5
otorquati <torquati.osvaldo@gmail.com>
2023-09-20 08:55:21 -0300

    Lesson 14: starting Register Page

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,6 +1,27 @@
 function RegisterPage() {
     return (
-        <h1>Register Page</h1>
+       <>
+        <form>
+                <div>
+                    <div><label>Email</label></div>
+                    <input type="email" placeholder="seu@email.com" />
+                </div>
+                <div>
+                    <div><label>Senha</label></div>
+                    <input type="passsword" placeholder="Senha" />
+                </div>
+                <div>
+                    <div><label>Email</label></div>
+                    <input type="password" placeholder="Confirmar senha" />
+                </div>
+                <div>
+                    <button type="button">Registrar</button>
+                </div>
+                <div>
+                    <button type="button">Login</button>
+                </div>
+        </form>
+       </>
     )
 }
 
